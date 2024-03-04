602ce72445fa43d7fe01e2f25ffb2e26bb958ad8
roneyyb <bothra.rajat08@gmail.com>
2024-03-04 11:11:07 +0530

    refactor: example code refactoring

--- example/src/screens/collapsable-list/views/PrimaryView.tsx
+++ example/src/screens/collapsable-list/views/PrimaryView.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import TextNormal from '../../../component/text/TextNormal'
 import ButtonPressableWithText from '../../../component/button/ButtonPressableWithText'
@@ -15,88 +15,93 @@ const PrimaryView = ({
     stepDetails: { by, title, step, image, duration, description },
 }: PrimaryViewProps) => {
     return (
-        <View style={{ flex: 1 }}>
+        <View style={styles.container}>
             <Image
-                source={{
-                    uri: image,
-                }}
-                style={{ height: '50%', width: '100%' }}
+                source={{ uri: image }}
+                style={styles.image}
                 resizeMode="cover"
             />
-            <View style={{ marginLeft: 20, marginTop: 10 }}>
-                <TextNormal
-                    text={`Step ${step}`}
-                    textStyle={{
-                        color: '#222222',
-                        fontSize: 15,
-                        fontWeight: '600',
-                    }}
-                />
-                <View style={{}}>
-                    <TextNormal
-                        text={title}
-                        textStyle={{
-                            color: 'rgba(4,4,4,255)',
-                            fontSize: 20,
-                            fontWeight: '600',
-                        }}
-                    />
-                    <TextNormal
-                        text={`By ${by}`}
-                        textStyle={{
-                            color: 'rgba(30,40,49,255)',
-                            fontSize: 10,
-                            fontWeight: '600',
-                        }}
-                    />
-
+            <View style={styles.contentContainer}>
+                <TextNormal text={`Step ${step}`} textStyle={styles.stepText} />
+                <View>
+                    <TextNormal text={title} textStyle={styles.titleText} />
+                    <TextNormal text={`By ${by}`} textStyle={styles.byText} />
                     <TextNormal
                         text={duration}
-                        textStyle={{ color: '#80858d' }}
+                        textStyle={styles.durationText}
                     />
                 </View>
                 <TextNormal
                     text={description}
-                    textStyle={{
-                        color: '#0b0b0b',
-
-                        marginTop: 10,
-                        fontWeight: 400,
-                    }}
+                    textStyle={styles.descriptionText}
                 />
             </View>
-            <View
-                style={{
-                    flex: 1,
-                    marginHorizontal: 20,
-
-                    // alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
+            <View style={styles.buttonContainer}>
                 <ButtonPressableWithText
-                    buttonStyle={{
-                        height: 50,
-                        width: '100%',
-                        backgroundColor: '#5498c9',
-                        elevation: 10,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        borderRadius: 10,
-                    }}
-                    text={'Next Step'}
-                    textStyle={{
-                        color: '#FFFFFF',
-                        fontSize: 16,
-                        fontWeight: 600,
-                    }}
-                    onPress={() => {
-                        onNext()
-                    }}
+                    buttonStyle={styles.buttonStyle}
+                    text="Next Step"
+                    textStyle={styles.buttonText}
+                    onPress={onNext}
                 />
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        height: '50%',
+        width: '100%',
+    },
+    contentContainer: {
+        marginLeft: 20,
+        marginTop: 10,
+    },
+    stepText: {
+        color: '#222222',
+        fontSize: 15,
+        fontWeight: '600',
+    },
+    titleText: {
+        color: 'rgba(4,4,4,255)',
+        fontSize: 20,
+        fontWeight: '600',
+    },
+    byText: {
+        color: 'rgba(30,40,49,255)',
+        fontSize: 10,
+        fontWeight: '600',
+    },
+    durationText: {
+        color: '#80858d',
+    },
+    descriptionText: {
+        color: '#0b0b0b',
+        marginTop: 10,
+        fontWeight: '400',
+    },
+    buttonContainer: {
+        flex: 1,
+        marginHorizontal: 20,
+        justifyContent: 'center',
+    },
+    buttonStyle: {
+        height: 50,
+        width: '100%',
+        backgroundColor: '#5498c9',
+        elevation: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+})
 
 export default PrimaryView
--- example/src/screens/collapsable-list/views/SecondaryView.tsx
+++ example/src/screens/collapsable-list/views/SecondaryView.tsx
@@ -1,6 +1,12 @@
 import { AntDesign } from '@expo/vector-icons'
 import React from 'react'
-import { Image, Pressable, View, type ViewStyle } from 'react-native'
+import {
+    Image,
+    Pressable,
+    StyleSheet,
+    View,
+    type ViewStyle,
+} from 'react-native'
 import TextNormal from '../../../component/text/TextNormal'
 import type { IYogaData } from '../data'
 
@@ -20,75 +26,69 @@ const SecondaryView = ({
             onPress={() => {
                 onNext()
             }}
-            style={[
-                containerStyle,
-                {
-                    margin: 10,
-                    padding: 10,
-                    // borderRadius: 10,
-                    backgroundColor: '#FFFFFF',
-                    elevation: 10,
-                    flexDirection: 'row',
-                    borderRadius: 10,
-                    alignSelf: 'center',
-                },
-            ]}
+            style={[styles.container, containerStyle]}
         >
-            <Image
-                source={{
-                    uri: image,
-                }}
-                style={{ height: '100%', aspectRatio: 1, borderRadius: 10 }}
-            />
-
-            <View
-                style={{
-                    flex: 1,
-                    marginLeft: 10,
-                }}
-            >
-                <TextNormal
-                    text={`Step ${step}`}
-                    textStyle={{
-                        color: '#222222',
-                        fontSize: 20,
-                        fontWeight: '600',
-                    }}
-                />
+            <Image source={{ uri: image }} style={styles.image} />
+            <View style={styles.contentContainer}>
+                <TextNormal text={`Step ${step}`} textStyle={styles.stepText} />
                 <View style={{ marginTop: 10 }}>
-                    <TextNormal
-                        text={by}
-                        textStyle={{
-                            color: 'rgba(30,40,49,255)',
-                            fontSize: 10,
-                            fontWeight: '600',
-                        }}
-                    />
-                    <TextNormal
-                        text={title}
-                        textStyle={{
-                            color: 'rgba(4,4,4,255)',
-                            fontSize: 16,
-                            fontWeight: '600',
-                        }}
-                    />
+                    <TextNormal text={by} textStyle={styles.byText} />
+                    <TextNormal text={title} textStyle={styles.titleText} />
                     <TextNormal
                         text={duration}
-                        textStyle={{ color: '#80858d' }}
+                        textStyle={styles.durationText}
                     />
                 </View>
             </View>
-            <View
-                style={{
-                    padding: 10,
-                    backgroundColor: '#eaf6ff',
-                    justifyContent: 'center',
-                }}
-            >
+            <View style={styles.arrowContainer}>
                 <AntDesign name="caretright" size={24} color="#5498c9" />
             </View>
         </Pressable>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        padding: 10,
+        backgroundColor: '#FFFFFF',
+        elevation: 10,
+        flexDirection: 'row',
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    image: {
+        height: '100%',
+        aspectRatio: 1,
+        borderRadius: 10,
+    },
+    contentContainer: {
+        flex: 1,
+        marginLeft: 10,
+    },
+    stepText: {
+        color: '#222222',
+        fontSize: 20,
+        fontWeight: '600',
+    },
+    byText: {
+        color: 'rgba(30,40,49,255)',
+        fontSize: 10,
+        fontWeight: '600',
+    },
+    titleText: {
+        color: 'rgba(4,4,4,255)',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+    durationText: {
+        color: '#80858d',
+    },
+    arrowContainer: {
+        padding: 10,
+        backgroundColor: '#eaf6ff',
+        justifyContent: 'center',
+    },
+})
 
 export default SecondaryView
